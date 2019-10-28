1df038752d999fa2f00b5b1183150637b2b9c1b2
Nathalie Solis-Maravilla <nathaliesolis-maravilla@Nathalies-MacBook-Pro.local>
2019-10-27 19:17:40 -0600

    server.js

--- server.js
+++ server.js
@@ -1,45 +1,21 @@
-var express  = require("express")
-var bodyParser = require('body-parser');
-var methodOverride = require('method-override')
+var express = require("express");
+var bodyParser = require("body-parser");
+var exphbs = require("express-handlebars");
 
-
-// bring in the models
-var models = require('./models')
-
-// sync the models
-models.sequelize.sync();
-
-// Instantiate Express
 var app = express();
-//Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static('public'));
+var PORT = process.env.PORT || 3000;
 
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({
-	extended: false
-}));
-// override with POST having ?_method=DELETEs
-app.use(methodOverride('_method'))
-var exphbs = require('express-handlebars');
-app.engine('handlebars', exphbs({
-    defaultLayout: 'main'
-}));
-app.set('view engine', 'handlebars');
+app.use(express.static("public"));
 
-// bring in the routes
-var routes = require("./controllers/burgers_controller");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-// connect the routes
-app.use('/', routes);
-app.use('/update', routes);
-app.use('/create', routes);
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
+var routes = require("./controllers/burgers_controller.js");
+app.use(routes);
 
-
-// listen on port 3000
-var PORT = process.env.PORT || 8000;
-app.listen(PORT, function(){
-    console.log("server listening on http://localhost:" + PORT)
+app.listen(PORT, function() {
+  console.log("Server listening on: http://localhost:" + PORT);
 });
-
-console.log(module.exports)
