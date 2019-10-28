5f31ead00889e4df622eed254f0845f72f73e404
Nathalie Solis-Maravilla <nathaliesolis-maravilla@Nathalies-MacBook-Pro.local>
2019-10-27 18:34:37 -0600

    changed some code in controller

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.engine('handlebars', exphbs({
 app.set('view engine', 'handlebars');
 
 // bring in the routes
-var routes = require("./controllers/burgers_controllers");
+var routes = require("./controllers/burgers_controller");
 
 // connect the routes
 app.use('/', routes);
