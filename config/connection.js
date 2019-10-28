0ed62c68f128e346752bab1859b005d6010d922e
Nathalie Solis-Maravilla <nathaliesolis-maravilla@Nathalies-MacBook-Pro.local>
2019-10-27 18:28:17 -0600

    better app

--- config/connection.js
+++ config/connection.js
@@ -1,18 +1,13 @@
-var mysql = require('mysql');
+var mysql = require("mysql");
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+  } else {
 var connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: 'password',
     database: 'burgers_db'
-});
-
-
-connection.connect(function(err) {
-    if (err) {
-        console.error('error connecting: ' + err.stack);
-        return;
-    }
-    console.log('connected as id ' + connection.threadId);
-});
-
+   });
+}
+connection.connect();
 module.exports = connection;
